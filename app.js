8e044f5dfeb69983fd65666a6dfd561d9ad276eb
ImrenR <imrenrahba@gmail.com>
2025-03-23 21:21:34 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+ // console.log(); // global 
+
+ // setTimeout()
+ // clearTimeout();
+
+ //setInterval()
+ // clearInterval() // all these functions globally belongs to window object
+
+//! In Node we dont have window obkect, instead we have "global"
+
+
+console.log(module);
