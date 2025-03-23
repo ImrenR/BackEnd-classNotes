4de72b33c34b9bcabd1dbb98ca44323a022f7e8a
ImrenR <imrenrahba@gmail.com>
2025-03-23 21:32:28 +0100

    p

--- logger.js
+++ logger.js
@@ -9,4 +9,3 @@ console.log(message);
 // so we need to make it local and outside
 
 module.export.log = log; // so I am adding metod called "log" there is exports object and simply setting into function log
-module.exports.endPoint = url;
