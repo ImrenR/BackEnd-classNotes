05ea29a6e11800bc9bc0a8a763b69c33067c8d86
ImrenR <imrenrahba@gmail.com>
2025-04-03 20:49:32 +0200

    up

---
+++ BE10- Sequelize/app.js
@@ -0,0 +1,26 @@
+const express= require('express');
+const app= express();
+
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
+
+/*------------------------*/
+// Accept json data
+/*------------------------*/
+app.use(express.json());
+
+app.all ('/', (req, res) => {
+    res.send('Hello World');
+}
+);
+/*------------------------*/
+
+/*------------------------*/
+
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode =res.errorStatusCode ?? 500;
+  res.status (errorStatusCode).send ({
+    error: true, // true if error
+    message: err.message, // error string message
+    cause : err.cause, // error cause
+  })}
