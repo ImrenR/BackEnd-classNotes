e6423d58a8e789643e5a862d2fc2a1cb51e7fe65
ImrenR <imrenrahba@gmail.com>
2025-04-06 13:05:36 +0200

    final

--- BE10- Sequelize/router/todo.router.js
+++ BE10- Sequelize/router/todo.router.js
@@ -10,12 +10,22 @@ router.get('/todos', todoController.list);
 
 //* CRUD OPERATIONS
 
-router.post('/todos', todoController.create);
+// router.post('/todos', todoController.create);
 
-router.get("/todos", todoController.read);
+// router.get("/todos", todoController.read);
 
-router.put('/todos/:id', todoController.update);
+// router.put('/todos/:id', todoController.update);
 
-router.delete('/todos/:id', todoController.delete);
+// router.delete('/todos/:id', todoController.delete);
+
+router.route ('/todos')
+  .get(todo.list)
+  .post(todo.create);
+ router.route('/todos/:id')
+  .get(todo.read)
+  .put(todo.update)
+  .delete(todo.delete);
+
+
 
 modules.exports = router;
