7b6bf6ac0e2f3e3b6c73e649bfc5074a24336bf4
ImrenR <imrenrahba@gmail.com>
2025-04-09 21:56:43 +0200

    up

--- BE13-Mongoose/src/dbConnection.js
+++ BE13-Mongoose/src/dbConnection.js
@@ -4,6 +4,8 @@
 // Importing mongoose
 const mongoose = require('mongoose');  
 
-const dbConnection = ()=> {
-  mongoose.connect
+const dbConnection = () => {
+  mongoose.connect('mongodb://localhost:27017/blogAPI')
+  .then(()=>console.log("Database connected successfully"))
+  .catch((err)=>console.log("Database connection failed", err));
 }
