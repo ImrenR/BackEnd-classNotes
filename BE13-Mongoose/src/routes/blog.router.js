8398d240b6c6503852745637c4f2d84b67cffb1a
ImrenR <imrenrahba@gmail.com>
2025-04-10 22:05:58 +0200

    up

--- BE13-Mongoose/src/routes/blog.router.js
+++ BE13-Mongoose/src/routes/blog.router.js
@@ -7,10 +7,3 @@ router.route('/blogs')
 .get(BlogCategory.list)
 .post(BlogCategory.create);
 module.exports = router;
-// router.route('/blogs').post(BlogCategory.create);
-// router.route('/blogs/:id').put(BlogCategory.update);
-// router.route('/blogs/:id').delete(BlogCategory.delete);
-// router.route('/blogs/:id').get(BlogCategory.list);
-// router.route('/blogs').get(BlogCategory.list);
-// router.route('/blogs').post(BlogCategory.create);
-// router.route('/blogs/:id').put(BlogCategory.update);
