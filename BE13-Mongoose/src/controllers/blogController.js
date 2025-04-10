1a061e94135b43e4ee21936f54a153fb5ad3e869
ImrenR <imrenrahba@gmail.com>
2025-04-10 21:36:42 +0200

    up

--- BE13-Mongoose/src/controllers/blogController.js
+++ BE13-Mongoose/src/controllers/blogController.js
@@ -7,11 +7,11 @@ module.exports = {
 
   list: async(req,res)=>{
    
-    const result = await BlogCategory
+    const result = await BlogCategory.find();
    
     res.status(200).send({
       error:false,
-      result
+      result:result
     });
   },
  create: async(req,res)=>{
