842e2d896842e0de94b9c4b427d550d0fc0e4f24
ImrenR <imrenrahba@gmail.com>
2025-04-12 13:18:40 +0200

    up

--- BE14-Blogpi_UserModel/src/controllers/blogController.js
+++ BE14-Blogpi_UserModel/src/controllers/blogController.js
@@ -3,7 +3,8 @@
 //Call Models:
 const {BlogCategory, BlogPost} = require("../models/blog.model") // import BlogCategory and BlogPost from blog.model
 
-module.exports = {
+//BlogCategory Controller
+module.exports.blogCategory = {
 
   list: async(req,res)=>{
    
@@ -51,6 +52,76 @@ update: async(req,res)=>{
     error:false,
     result,
     new: await BlogCategory.findById(req.params.id)
+  });
+},
+
+delete: async (req,res)=>{
+   
+  const result = await BlogCategory.deleteOne({_id:req.params.id});
+  // const result = await BlogCategory.findByIdAndDelete(req.params.id)
+  // const result = await BlogCategory.findByIdAndRemove(req.params.id)
+ 
+  if(result.deletedCount === 1){
+    res.status(204);
+  }else {
+     res.errorStatuscode =404;
+     throw new Error ('Blog not found');
+  }
+  res.status(200).send({
+    error:false,
+    result
+  });
+},
+}
+// BlogPost Controller
+module.exports.blogPost = {
+
+  list: async(req,res)=>{
+   
+    const result = await BlogPost.find();
+   
+    res.status(200).send({  
+      error:false,
+      result
+    });
+  },
+
+ create: async(req,res)=>{
+   
+    const result = await BlogPost.create(req.body);
+   
+    res.status(201).send({
+      error:false,
+      result
+    });
+  },
+
+  read: async(req,res)=>{
+   
+    const result = await BlogPost.findById(req.params.id);
+   
+    res.status(200).send({
+      error:false,
+      result
+    });
+  },
+
+update: async(req,res)=>{
+   //acknowledged: true, // if update is successful
+  //modifiedCount: 1, // number of documents modified
+  //upsertedId: null, // id of the inserted document
+  //upsertedCount: 0, // number of documents inserted
+  //matchedCount: 1 // number of documents matched
+  
+  
+  // const result = await BlogPost.updateOne({_id:req.params.id}, req.body)
+  const result = await BlogPost.findOneAndUpdate({_id:req.params.id}, req.body, {new:true})
+
+ 
+  res.status(200).send({
+    error:false,
+    result,
+    new: await BlogPost.findById(req.params.id)
   });
 },
 
--- BE14-Blogpi_UserModel/src/routes/blog.router.js
+++ BE14-Blogpi_UserModel/src/routes/blog.router.js
@@ -1,16 +1,26 @@
 'use strict';
 
 const router = require('express').Router();
-const BlogCategory = require('../controllers/blogController');
-
-router.route('/blogs')
+const {BlogCategory, blogPost} = require('../controllers/blogController');
+//BlogCategory
+router.route('/blogs/category')
 .get(BlogCategory.list)
 .post(BlogCategory.create);
 
-router.route('/blogs/:id')
+router.route('/blogs/category/:id')
 .get(BlogCategory.read)
 .put(BlogCategory.update)
 .delete(BlogCategory.delete);
+
+//BlogPostposd
+router.route('/blogs/post')
+.get(blogPost.list)
+.post(blogPost.create);
+
+router.route('/blogs/post/:id')
+.get(blogPost.read)
+.put(blogPost.update)
+.delete(blogPost.delete);
 
 module.exports = router;
  
