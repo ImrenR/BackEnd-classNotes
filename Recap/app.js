7caf57b64f29c757c6baed7924d5aafd4cb1ec1b
ImrenR <imrenrahba@gmail.com>
2025-04-13 12:42:15 +0200

    up

--- Recap/app.js
+++ Recap/app.js
@@ -4,11 +4,16 @@ const express= require('express');
 const app= express();
 const PORT = process.env?.PORT || 8000;
 require('dotenv').config();
- app.use(express.json());
- require('express-async-errors'); 
+app.use(express.json());
+require('express-async-errors'); 
+
 
 
 app.all('/',(req,res)=> res.send('welcome'));
+app.use(require('./src/middlewares/errorHandlers'));
+
+
+
 
 
 app.listen(PORT, ()=> console.log(`Running at ${PORT}`));
