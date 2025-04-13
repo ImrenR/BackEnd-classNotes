8041d755e0490e119ec55095ee523b76f60acae1
ImrenR <imrenrahba@gmail.com>
2025-04-13 18:52:10 +0200

    up

--- Recap/src/models/blog.model.js
+++ Recap/src/models/blog.model.js
@@ -1,3 +1,48 @@
 'use strict';
 
- 
+  const {mongoose }= require('mongoose');
+
+  const blogCategorySchema = new mongoose.Schema({
+    // _id is default
+       name: {
+        type : String,
+        trim: true,
+        required: true,
+        unique: true
+       }
+
+  }, {
+    collection: 'blogCategories',
+    timestamps : true
+  })
+
+  const blogPostSchema = new mongoose.Schema({
+    // _id is default
+       categoryId: { // default relation ManyToOne
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'BlogCategory', // gave model name not collection name!
+        required: true,
+        unique: true
+       },
+       title:{
+        type : String,
+        trim: true,
+        required: true,
+        unique: true
+      
+       },
+       content:{
+        type : String,
+        trim: true,
+        required: true,
+        unique: true
+       },
+
+
+  }, {
+    collection: 'blogPosts',
+    timestamps : true
+  })
+
+/* Convert Schema to Model  */
+  module.exports = mongoose.model('BlogCategory', blogCategorySchema);
