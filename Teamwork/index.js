4726b6986d41b897d8fe9a09b17c7e132d446a8f
ImrenR <imrenrahba@gmail.com>
2025-04-13 13:24:01 +0200

    up

--- Teamwork/index.js
+++ Teamwork/index.js
@@ -1,21 +1,42 @@
- const express = require ('express'); //Express kutuphanesini projeye dahil ettim
- const app = express (); // Express uygulamami baslattim
- const PORT = 8000;
+'use strict';
 
-// 9. soru Middleware ile gelen istegi logla
-// asagida gorulen bir middleware fonksiyonudur
-function logRequest (req,res, next) {
-  console.log(`$ {req.method} ${req.url}`);
-   next();
-}
-app.use(logRequest);
+const express = require('express');
+const app = express();
+const PORT = process.env?.PORT || 8000;
 
+require('dotenv').config();
 
-app.listen(PORT, ()=> console.log(`Running at: http://127.0.0.1:${PORT}`))
-// Test routelari 
+const users = [
+  {name : 'imren', age: 22},
+  {name : 'kelvin', age: 32},
+  {name : 'melvin', age: 42},
+]
 
-app.get ('/', (req,res) =>{
-  res.send ('Anasayfa')
-})
+const posts = [
+  {title:'My favorite foods'},
+  {title:'My favorite games'}
+]
+app.get('/', (req,res)=> {
+  res.send({
+    msg: 'Hi There',
+    user : {}
+  })
+});
 
+app.get('/users', (req,res)=> {
+  res.status(200).send(users)
+});
 
+app.get('/posts', (req,res)=> {
+  const quer = posts.find(a => a.title === req.query.title);
+  res.status(quer? 200 :404).send(quer ?? 'Fav title not found, try again!');
+});
+
+app.get('/users/:name', (req,res)=> {
+
+  const user = users.find((u=> u.name === req.params.name));
+
+  res.status(user ? 200 :404).send(user ?? 'User not found, try again!');
+});
+
+app.listen(PORT,()=> console.log(`Running at ${PORT}`))
