dd48c9ae6f632c8940ebfa0d3efd142977b7ef18
ImrenR <imrenrahba@gmail.com>
2025-04-15 20:36:22 +0200

    router

--- Recap/src/controllers/blog.controller.js
+++ Recap/src/controllers/blog.controller.js
@@ -3,6 +3,53 @@
 const {BlogCategory, BlogPosts} = require('../models/blog.model');
 
 module.exports = {
-  list
+  list : async (req,res)=> {
 
+const result = await BlogCategory.find(req.body);
+
+  res.status(200).send({
+        error:false,
+        result
+  });
+},
+
+create : async (req,res)=> {
+
+  const result = await BlogCategory
+  
+    res.status(200).send({
+          error:false,
+          result
+    });
+  },
+
+  read : async (req,res)=> {
+
+    const result = await BlogCategory
+    
+      res.status(200).send({
+            error:false,
+            result
+      });
+    },
+
+    update : async (req,res)=> {
+
+      const result = await BlogCategory
+      
+        res.status(200).send({
+              error:false,
+              result
+        });
+      },
+
+      delete: async (req,res)=> {
+
+        const result = await BlogCategory
+        
+          res.status(200).send({
+                error:false,
+                result
+          });
+        },
 }
