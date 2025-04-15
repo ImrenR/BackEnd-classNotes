a972b193be80297ca8cfe86ae782c3f87a97c6bd
ImrenR <imrenrahba@gmail.com>
2025-04-15 22:01:50 +0200

    userSchema

--- Recap/src/controllers/blog.controller.js
+++ Recap/src/controllers/blog.controller.js
@@ -1,60 +1,56 @@
 "use strict";
 
-const {BlogCategory, BlogPosts} = require('../models/blog.model');
+const { BlogCategory, BlogPosts } = require("../models/blog.model");
 
 module.exports = {
-  list : async (req,res)=> {
+  list: async (req, res) => {
+    const result = await BlogCategory.find();
 
-const result = await BlogCategory.find( );
-
-  res.status(200).send({
-        error:false,
-        result
-  });
-},
-
-create : async (req,res)=> {
-
-  const result = await BlogCategory.create(req.body)
-  
-    res.status(201).send({
-          error:false,
-          result
+    res.status(200).send({
+      error: false,
+      result,
     });
   },
 
-  read : async (req,res)=> {
+  create: async (req, res) => {
+    const result = await BlogCategory.create(req.body);
 
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
     const result = await BlogCategory.findById(req.params.id);
-    
-      res.status(200).send({
-            error:false,
-            result
-      });
-    },
 
-    update : async (req,res)=> {
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
 
-      const result = await BlogCategory.updateOne({_id:req.params.id},req.body)
-      // findOneAndUpdate {new : true}  another method
-        res.status(200).send({
-              error:false,
-              result,
-              new: await BlogCategory.findById(req.params.id)
-        });
-      },
+  update: async (req, res) => {
+    const result = await BlogCategory.updateOne(
+      { _id: req.params.id },
+      req.body
+    );
+    // findOneAndUpdate {new : true}  another method
+    res.status(200).send({
+      error: false,
+      result,
+      new: await BlogCategory.findById(req.params.id),
+    });
+  },
 
-      delete: async (req,res)=> {
+  delete: async (req, res) => {
+    const result = await BlogCategory.deleteOne({ _id: req.params.id });
 
-        const result = await BlogCategory.deleteOne({_id:req.params.id});
-        
-        if(result.deletedCount){
-          res.send(204);
-          }else {
-            res.errorStatusCode = 404;
-            throw new Error('Data is not found');
-          }
-        }
-          
-        },
-}
+    if (result?.deletedCount) {
+      res.send(204);
+    } else {
+      res.errorStatusCode = 404;
+      throw new Error("Data is not found");
+    }
+  },
+};
---
+++ Recap/src/models/user.model.js
@@ -0,0 +1,8 @@
+'use strict';
+const mongoose = require ('mongoose');
+const UserSchema = new mongoose.Schema ({
+
+}, {
+  collection:'username',
+  timestamps:true
+})
