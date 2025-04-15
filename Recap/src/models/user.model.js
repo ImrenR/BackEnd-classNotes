d29bff88ecef64e0c12fed87537e35db03f64f57
ImrenR <imrenrahba@gmail.com>
2025-04-15 22:48:05 +0200

    Encryption

--- Recap/src/models/user.model.js
+++ Recap/src/models/user.model.js
@@ -1,6 +1,17 @@
 "use strict";
 const mongoose = require("mongoose");
-const crypto = require ('cyrpto');
+const crypto = require('crypto');
+
+const passwordEncrypte = (password) => {
+  const salt = '223894fshjfg23947hjs';
+  const iteration = 10000;
+  const keylen = 100;
+  const digest = 'sha512';
+  return crypto.pbkdf2Sync(password,salt,iteration,keylen,digest).toString('hex'); 
+
+  
+}
+
 const UserSchema = new mongoose.Schema(
   {
     email: {
